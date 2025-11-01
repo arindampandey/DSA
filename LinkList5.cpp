75e5aa2e05513314e1df4985906e01f8ff620a2e
Arindam Pandey <arindampandey63@gmail.com>
2025-11-01 14:12:20 +0530

    Add files via upload

---
+++ LinkList5.cpp
@@ -0,0 +1,68 @@
+// Delete element from beginning
+#include <iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *next;
+};
+Node *head = 0, *temp;
+
+void Delete_from_beginning()
+{
+    temp = head;
+    head = head->next;
+    free(temp);
+}
+
+void Insert_at_end()
+{
+    Node *newnode = new Node;
+    cout << "Enter the data that you want to insert : ";
+    cin >> newnode->data;
+    newnode->next = 0;
+    if (head == 0)
+    {
+        head = newnode;
+        temp = head;
+    }
+    else
+    {
+        temp = head;
+        while (temp->next != 0)
+        {
+            temp = temp->next;
+        }
+        temp->next = newnode;
+    }
+}
+
+void display()
+{
+    if (head == 0)
+    {
+        cout << "List is empty." << endl;
+        return;
+    }
+
+    temp = head;
+    while (temp != 0)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int choice;
+    do
+    {
+        Insert_at_end();
+        cout << "Do you want to continue ? (0/1) : ";
+        cin >> choice;
+    } while (choice);
+    Delete_from_beginning();
+    display();
+}
