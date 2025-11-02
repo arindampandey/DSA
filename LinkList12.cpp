9da1b1408ec9ab9aadc281f401717196dc05d2ff
Arindam Pandey <arindampandey63@gmail.com>
2025-11-02 20:50:17 +0530

    Add files via upload

---
+++ LinkList12.cpp
@@ -0,0 +1,73 @@
+// Insertion at end
+#include <iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *next, *previous;
+};
+Node *head = 0, *newnode, *tail, *temp;
+
+void Insert_at_end()
+{
+    Node *newnode = new Node;
+    cout << "Enter data to add at the end : ";
+    cin >> newnode->data;
+    newnode->previous = 0;
+    newnode->next = 0;
+    tail->next = newnode;
+    newnode->previous = tail;
+    tail = newnode;
+}
+
+void create()
+{
+    Node *newnode = new Node;
+    cout << "Enter data : ";
+    cin >> newnode->data;
+    newnode->previous = 0;
+    newnode->next = 0;
+    if (head == 0)
+    {
+        head = tail = newnode;
+    }
+    else
+    {
+        tail->next = newnode;
+        newnode->previous = tail;
+        tail = newnode;
+    }
+}
+
+void display()
+{
+    if (head == 0)
+    {
+        cout << "The linklist is empty!!";
+    }
+    else
+    {
+        temp = head;
+        while (temp != 0)
+        {
+            cout << temp->data << endl;
+            temp = temp->next;
+        }
+    }
+}
+
+int main()
+{
+    int choice;
+    do
+    {
+        create();
+        cout << "Do you want to continue ? (0/1) : ";
+        cin >> choice;
+    } while (choice);
+
+    display();
+    Insert_at_end();
+    display();
+    return 0;
+}
