4f01cd250d59a79b0a11e3f06080c6ad6233c698
Arindam Pandey <arindampandey63@gmail.com>
2025-11-03 10:14:39 +0530

    Add files via upload

---
+++ LinkedList15.cpp
@@ -0,0 +1,70 @@
+// Delete element from beginning in doubly LinkedList
+#include <iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *next, *previous;
+};
+Node *head = 0, *newnode, *tail, *temp;
+
+void Delete_from_beginning()
+{
+    temp = head;
+    head = head->next;
+    head->previous = 0;
+    free(temp);
+}
+
+void create()
+{
+    Node *newnode = new Node;
+    cout << "Enter data : ";
+    cin >> newnode->data;
+    newnode->previous = 0;
+    newnode->next = 0;
+    if (head == 0)
+    {
+        head = tail = newnode;
+    }
+    else
+    {
+        tail->next = newnode;
+        newnode->previous = tail;
+        tail = newnode;
+    }
+}
+
+void display()
+{
+    if (head == 0)
+    {
+        cout << "The linklist is empty!!";
+    }
+    else
+    {
+        temp = head;
+        while (temp != 0)
+        {
+            cout << temp->data << endl;
+            temp = temp->next;
+        }
+    }
+}
+
+int main()
+{
+    int choice;
+    do
+    {
+        create();
+        cout << "Do you want to continue ? (0/1) : ";
+        cin >> choice;
+    } while (choice);
+
+    display();
+    Delete_from_beginning();
+    cout << "After deleting element from beginning : ";
+    display();
+    return 0;
+}
