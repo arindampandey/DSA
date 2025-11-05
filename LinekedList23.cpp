c9bd34cfa4878834ee01a24731f6b6e2ffd31d82
Arindam Pandey <arindampandey63@gmail.com>
2025-11-06 00:02:30 +0530

    Add files via upload

---
+++ LinekedList23.cpp
@@ -0,0 +1,82 @@
+// Insertion at end in a Singly Circular LinkedList
+#include <iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+Node *tail = 0, *newnode;
+
+void Insert_at_end()
+{
+    Node *newnode = new Node;
+    cout << "Enter data : ";
+    cin >> newnode->data;
+    newnode->next = 0;
+    if (tail == 0)
+    {
+        tail = newnode;
+        tail->next = newnode;
+    }
+    else
+    {
+        newnode->next = tail->next;
+        tail->next = newnode;
+        tail = newnode;
+    }
+}
+
+void create()
+{
+    Node *newnode = new Node;
+    cout << "Enter data : ";
+    cin >> newnode->data;
+    newnode->next = 0;
+    if (tail == 0)
+    {
+        tail = newnode;
+        tail->next = newnode;
+    }
+    else
+    {
+        newnode->next = tail->next;
+        tail->next = newnode;
+        tail = newnode;
+    }
+}
+
+void display()
+{
+    Node *temp;
+    if (tail == 0)
+    {
+        cout << "The List Is Empty." << endl;
+    }
+    else
+    {
+        temp = tail->next;
+        while (temp->next != tail->next)
+        {
+            cout << temp->data << endl;
+            temp = temp->next;
+        }
+        cout << temp->data << endl;
+    }
+}
+
+int main()
+{
+    int choice;
+    do
+    {
+        create();
+        cout << "Do you want to continue ? (0/1) : ";
+        cin >> choice;
+    } while (choice);
+
+    display();
+    Insert_at_end();
+    display();
+}
