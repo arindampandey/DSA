bd444491b333a5d33ea85fc5b0445fd1206c0703
Arindam Pandey <arindampandey63@gmail.com>
2025-11-06 00:03:04 +0530

    Add files via upload

---
+++ LinekedList25.cpp
@@ -0,0 +1,83 @@
+// Delete element from beginning in a Singly Circular LinkedList
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+Node *tail, *temp;
+
+void Delete_from_beginning()
+{
+    temp = tail->next;
+    if (tail == 0)
+    {
+        cout << "The list is empty !" << endl;
+    }
+    else if (temp->next == temp)
+    {
+        tail = 0;
+        free(temp);
+    }
+    else
+    {
+        tail->next = temp->next;
+        free(temp);
+    }
+    cout << "After the deleting the element from beginning : " << endl;
+}
+
+void create()
+{
+    Node *newnode = new Node;
+    cout << "Enter data : ";
+    cin >> newnode->data;
+    newnode->next = 0;
+    if (tail == 0)
+    {
+        tail = newnode;
+        tail->next = newnode;
+    }
+    else
+    {
+        newnode->next = tail->next;
+        tail->next = newnode;
+        tail = newnode;
+    }
+}
+
+void display()
+{
+    if (tail == 0)
+    {
+        cout << "The List Is Empty." << endl;
+    }
+    else
+    {
+        temp = tail->next;
+        while (temp->next != tail->next)
+        {
+            cout << temp->data << endl;
+            temp = temp->next;
+        }
+        cout << temp->data << endl;
+    }
+}
+
+int main()
+{
+    int choice;
+    do
+    {
+        create();
+        cout << "Do you want to continue ? (0/1) : ";
+        cin >> choice;
+    } while (choice);
+    display();
+
+    Delete_from_beginning();
+    display();
+}
