16d7b60f6c1c7a9ae57d1ec361a35ae5db193350
Arindam Pandey <arindampandey63@gmail.com>
2025-11-06 00:03:48 +0530

    Add files via upload

---
+++ LinekedList28.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+Node *tail, *temp;
+
+void reverse()
+{
+    Node *current, *previous, *nextnode;
+    current = tail->next;
+    nextnode = current->next;
+    if (tail == 0)
+    {
+        cout << "The list is empty ! " << endl;
+    }
+    else
+    {
+        while (current != tail)
+        {
+            previous = current;
+            current = nextnode;
+            nextnode = current->next;
+            current->next = previous;
+        }
+        nextnode->next = tail;
+        tail = nextnode;
+    }
+    cout << "After reversing : " << endl;
+}
+
+void create()
+{
+    Node *newnode = new Node;
+    cout << "Enter data : ";
+    cin >> newnode->data;
+    newnode->next = 0;
+    if (tail == 0)
+    {
+        tail = newnode;
+        tail->next = newnode;
+    }
+    else
+    {
+        newnode->next = tail->next;
+        tail->next = newnode;
+        tail = newnode;
+    }
+}
+
+void display()
+{
+    if (tail == 0)
+    {
+        cout << "The List Is Empty." << endl;
+    }
+    else
+    {
+        temp = tail->next;
+        while (temp->next != tail->next)
+        {
+            cout << temp->data << endl;
+            temp = temp->next;
+        }
+        cout << temp->data << endl;
+    }
+}
+
+int main()
+{
+    int choice;
+    do
+    {
+        create();
+        cout << "Do you want to continue ? (0/1) : ";
+        cin >> choice;
+    } while (choice);
+    display();
+
+    reverse();
+    display();
+}
