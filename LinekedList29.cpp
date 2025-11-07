bfdff2b2800cd279c98158a90e2a1cbe3516dcf6
Arindam Pandey <arindampandey63@gmail.com>
2025-11-07 12:20:32 +0530

    Add files via upload

---
+++ LinekedList29.cpp
@@ -0,0 +1,67 @@
+// Implementation of Doubly Circular Linkedlist
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next, *previous;
+};
+
+Node *head=0, *tail;
+
+void create()
+{
+    Node *nextnode;
+    Node *newnode = new Node;
+    cout << "Enter data : ";
+    cin >> newnode->data;
+    if (head == 0)
+    {
+        head = tail = newnode;
+        head->next = head;
+        head->previous = head;
+    }
+    else
+    {
+        tail->next = newnode;
+        newnode->previous = tail;
+        newnode->next = head;
+        head->previous = newnode;
+        tail = newnode;
+    }
+}
+
+void display()
+{
+    Node *temp;
+    temp = head;
+    if (head == 0)
+    {
+        cout << "List is empty ! " << endl;
+    }
+    else
+    {
+        while (temp != tail)
+        {
+            cout << temp->data << endl;
+            temp = temp->next;
+        }
+        cout << temp->data << endl;
+    }
+}
+
+int main()
+{
+    int choice;
+    do
+    {
+        create();
+        cout << "Do you want to continue ? (0/1) : ";
+        cin >> choice;
+    } while (choice);
+
+    // cout << head->previous->data;
+    // cout << tail->next->data;
+    display();
+}
