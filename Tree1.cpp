c4a7fc7ef7fe9196d73065472a823d7a736eb2c3
Arindam Pandey <arindampandey63@gmail.com>
2025-11-24 00:15:04 +0530

    Add files via upload

---
+++ Tree1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left, *right;
+};
+
+Node *create()
+{
+    int x;
+    Node *newnode = new Node;
+    cout << "Enter data (-1 for no node): ";
+    cin >> x;
+    if (x == -1)
+    {
+        return 0;
+    }
+    newnode->data = x; 
+    cout << "For left child of " << x << " : " << endl;
+    newnode->left = create();
+    cout << "For right child of " << x << " : " << endl;
+    newnode->right = create();
+    return newnode;
+}
+
+int main()
+{
+    Node *root = 0;
+    root = create();
+    return 0;
+}
